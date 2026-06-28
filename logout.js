99fdc1423f0179ac84f7f733639d2d18c9f5a8e4
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-06-28 22:19:35 +0530

    working with logout route

--- logout.js
+++ logout.js
@@ -1,13 +1,21 @@
+const btn = document.querySelector(".btn-js");
 
-
-const btn = document.querySelector('.btn-js');
-
-btn.addEventListener('click', (e)=>{
+btn.addEventListener("click", async (e) => {
   e.preventDefault();
-  console.log('clicked');
 
-  localStorage.removeItem('loggedInUser');
+  try {
+    const response = await fetch("https://login-authentication-using-node-js-with.onrender.com/auth/logout", {
+      method: "POST",
+      credentials: "include"
+    });
 
-  window.location.replace('index.html');
+    const data = await response.json();
+    console.log(data);
 
+    localStorage.removeItem("loggedInUser");
+
+    window.location.replace("index.html");
+  } catch (err) {
+    console.error("Logout failed:", err);
+  }
 });
