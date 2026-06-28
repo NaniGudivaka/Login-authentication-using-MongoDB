5bba7f08e58b9c883206960835bc571a23801d42
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-06-28 15:32:08 +0530

    Adding backend api to frontend

--- login.js
+++ login.js
@@ -5,43 +5,82 @@ const errorMsg = document.getElementById('error-msg');
 
 
 //Dummy users dor checking 
-const users =[  {
-    email: "admin@gmail.com",
-    password: "admin123"
-  },
-  {
-    email: "john@gmail.com",
-    password: "john123"
-  },
-  {
-    email: "nani@gmail.com",
-    password: "nani123"
-  }]
+// const users =[  {
+//     email: "admin@gmail.com",
+//     password: "admin123"
+//   },
+//   {
+//     email: "john@gmail.com",
+//     password: "john123"
+//   },
+//   {
+//     email: "nani@gmail.com",
+//     password: "nani123"
+//   }]
 
-form.addEventListener('submit', (e) =>{
+form.addEventListener('submit', async (e) =>{
   e.preventDefault();
 
   errorMsg.textContent = '';
-  
 
-  if(email.value.trim() === '' ||
-password.value.trim() === ''){
-    alert("Please fill the all fields");
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if(!emailValue || !passwordValue){
+    errorMsg.textContent = 'All fields are required';
     return;
   }
+  try{
 
-  const loggedInUser = users.find((u) =>
-    u.email === email.value.trim() &&
-    u.password === password.value.trim()
+    const response = await fetch('https://login-authentication-using-node-js-with.onrender.com/auth/login',{
+      method: 'POST',
+      headers:{
+        "Content-Type": "application/json",
+      },
+      credentials:'include',
+      body: JSON.stringify({
+        email: emailValue,
+        password: passwordValue
+      }),
+    }
   );
-  if(!loggedInUser){
-    errorMsg.textContent = 'Invalid email or password';
+
+  const data = await response.json();
+
+  if(!response.ok){
+    errorMsg.textContent = data.message;
+    password.value = '';
     return;
   }
 
-  localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser));
+    window.location.replace('dashboard.html');
+
+  }catch(error){
+
+    errorMsg.textContent = 'Something went wrong, PLease tey again';
+    console.log(error);
+
+  }
+  
+
+//   if(email.value.trim() === '' ||
+// password.value.trim() === ''){
+//     alert("Please fill the all fields");
+//     return;
+//   }
+
+//   const loggedInUser = users.find((u) =>
+//     u.email === email.value.trim() &&
+//     u.password === password.value.trim()
+//   );
+//   if(!loggedInUser){
+//     errorMsg.textContent = 'Invalid email or password';
+//     return;
+//   }
+
+//   localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser));
 
   
-  window.location.href= 'dashboard.html';
+//   window.location.href= 'dashboard.html';
 
 });
--- signup.js
+++ signup.js
@@ -5,27 +5,70 @@ const passwordInput = document.getElementById('password');
 const errorMsg = document.getElementById('error-msg');
 const successMsg = document.getElementById('success-msg');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
   errorMsg.textContent = '';
   successMsg.textContent = '';
 
-  if (nameInput.value.trim() === '' ||
-    emailInput.value.trim() === '' ||
-    passwordInput.value.trim() === '') {
+  const name = nameInput.value.trim();
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
 
+  if(!name || !email || !password ){
     errorMsg.textContent = 'All fields are required';
     return;
+  }
+
+  try{
+    const response = await fetch('https://login-authentication-using-node-js-with.onrender.com/auth/signup',{
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        name,
+        email,
+        password
+      }),
+    });
+
+    const data = await response.json();
+
+    if(!response.ok){
+      errorMsg.textContent = data.message;
+      return;
+    }
+    successMsg.textContent = data.message;
+
+    form.reset();
+
+    setTimeout(() =>{
+      window.location.replace('index.html');
+    }, 300);
+
+  }catch(error){
+    errorMsg.textContent = 'Something went wrong. Please try again..';
+    console.log(error);
 
   }
 
-  successMsg.textContent = 'Details are saved';
+  // if (nameInput.value.trim() === '' ||
+  //   emailInput.value.trim() === '' ||
+  //   passwordInput.value.trim() === '') {
+
+  //   errorMsg.textContent = 'All fields are required';
+  //   return;
+
+  // }
+
+  // successMsg.textContent = 'Details are saved';
 
 
-  setTimeout(() => {
-    window.location.replace('index.html');
+  // setTimeout(() => {
+  //   window.location.replace('index.html');
 
-  }, 500);
+  // }, 500);
 
 });
